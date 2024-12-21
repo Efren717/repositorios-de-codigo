52a22a5be96ddf29694e34eff8afa7dbe69c1c3c
Efren717 <ruizflacko5@gmail.com>
2024-12-21 03:26:05 -0600

    Delete src directory

--- src/script.js
+++
@@ -1,6 +0,0 @@
-const boton_1 = document.getElementById('cambiarColor_1')
-
-boton_1.addEventListener('click', () => {
-  const body = document.querySelector('body')
-  body.style.backgroundColor = 'yellow'
-})
